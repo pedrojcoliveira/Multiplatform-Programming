b48c95cd2bccef6d070dc76065d233d68994f958
Pedro Oliveira <97547565+pedrojcoliveira@users.noreply.github.com>
2022-07-18 12:07:18 +0100

    Add files via upload

---
+++ Aula13IoT/Aula13IoT/Delegates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13IoT
+{
+    //Declaração de delegate multicast que vai ser associado a event
+    public delegate void MetedosComString(string valor);
+}
---
+++ Aula13IoT/Aula13IoT/ModelSomar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13IoT
+{
+    public class ModelSomar
+    {
+        //Propriedades que representa o estado da aplicação
+        public string Resultado { get; private set; }
+
+        //Declaração de event que irá notificar a View de alteração de estado
+        public event MetedosComString SomaTerminada;
+        public void Somar(string valor1,string valor2)
+        {
+            Resultado = (Convert.ToInt32(valor1) + Convert.ToInt32(valor2)).ToString();
+
+            if(SomaTerminada != null)
+                SomaTerminada(Resultado);
+        }
+    }
+}
