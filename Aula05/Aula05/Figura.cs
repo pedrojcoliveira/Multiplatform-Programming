733b0ad8eeefc1392671434b7988d760b8f72690
Pedro Oliveira <97547565+pedrojcoliveira@users.noreply.github.com>
2022-07-18 11:27:48 +0100

    Add files via upload

---
+++ Aula05/Aula05/Figura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05
+{
+    public class Figura
+    {
+        public string Nome { get; set; }
+        public string Largura { get; set; }
+
+        public string Altura { get; set; }
+
+    }
+}
---
+++ Aula05/Aula05/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aula05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List <Figura> Lista { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Lista = new List<Figura>(); 
+        }
+
+        private void FigurasAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAdicionar dlg = new WindowAdicionar();
+
+           if (dlg.ShowDialog() == true)
+            {
+                lbFiguras.Items.Add(dlg.NovaFigura.Nome);
+                Lista.Add(dlg.NovaFigura);
+            }
+        }
+
+        private void lbFiguras_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string largura = Lista[lbFiguras.SelectedIndex].Largura;
+            string altura = Lista[lbFiguras.SelectedIndex].Altura;
+
+            sbiFiguras.Content = "Largura= " + largura + "Altura = " + altura;
+
+                
+        }
+    }
+}
