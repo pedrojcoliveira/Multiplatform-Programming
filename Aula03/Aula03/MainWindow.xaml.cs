2ee89eaa3c1c3e3dcf44011fdcb09ce70c3b4e84
Pedro Oliveira <97547565+pedrojcoliveira@users.noreply.github.com>
2022-07-18 11:22:47 +0100

    Add files via upload

---
+++ Aula03/Aula03/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aula03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton(object sender, RoutedEventArgs e)
+        {
+            int num1, num2, sum;
+
+            num1 = Convert.ToInt32(FVBox.Text);
+            num2 = Convert.ToInt32(SVBox.Text);
+
+            sum = num1 + num2;
+            ResultBox.Text = sum.ToString();
+
+            MessageBox.Show($"Result {ResultBox.Text}");
+
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox.Items.Add(resultado);
+        }
+    }
+}
